d835626bfe47683c8168e9f209905c07391bb929
popoxta <popoxta@gmail.com>
2023-07-17 09:47:47 +1200

    Add header

---
+++ src/components/Header.tsx
@@ -0,0 +1,8 @@
+export default function Header() {
+    return (
+        <header>
+            <h1 className={'text-6xl font-bold text-amber-950'}><span className={'text-red-700'}>POMO</span>DORO 🍅</h1>
+            <h2 className={'text-xl font-medium self-start text-gray-600 pl-0.5'}>Your tomato timer buddy</h2>
+        </header>
+    )
+}
