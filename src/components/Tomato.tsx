4a9f968ea0056a2b5472a59c1973c1d22732ffc7
popoxta <popoxta@gmail.com>
2023-07-17 18:36:40 +1200

    Add dynamic quotes

--- src/components/Tomato.tsx
+++ src/components/Tomato.tsx
@@ -1,16 +1,22 @@
 import {useEffect, useState} from "react";
 
-export default function Tomato() {
+export default function Tomato({pomosElapsed}: { pomosElapsed: number }) {
+    const [allQuotes, setAllQuotes] = useState([] as { text: string }[])
     const [quote, setQuote] = useState('')
 
+    const isBreak = pomosElapsed % 2 !== 0
+
     useEffect(() => {
-        if (quote === '') {
-            fetch('https://type.fit/api/quotes',)
-                .then(res => res.json())
-                .then(quotes => quotes[Math.floor(Math.random() * quotes.length)])
-                .then(quote => setQuote(quote.text))
-        }
+        if (allQuotes.length) return
+        fetch('https://type.fit/api/quotes',)
+            .then(res => res.json())
+            .then(quotes => setAllQuotes(quotes))
     }, [])
+
+    useEffect(() => {
+        if (!allQuotes?.length || isBreak) return
+        setQuote(allQuotes[Math.floor(Math.random() * allQuotes.length)].text)
+    }, [pomosElapsed, allQuotes])
 
     const tomatoes: { rest: string, work: string } = {
         rest: '../../tomato-rest.png',
@@ -18,11 +24,11 @@ export default function Tomato() {
     }
 
     return (
-        <section className={'flex flex-row gap-5 justify-center place-items-center mt-24 mb-20'}>
-            <img className={'w-60'} src={tomatoes.work} alt={'Cute Pomodoro'}/>
+        <div className={'flex flex-row gap-5 justify-center place-items-center mt-24 mb-20'}>
+            <img className={'w-60'} src={isBreak ? tomatoes.rest : tomatoes.work} alt={'Cute Pomodoro'}/>
             <div className={'bg-slate-200 mt-3 px-6 py-7 rounded-lg w-80'}>
                 <p>{quote}</p>
             </div>
-        </section>
+        </div>
     )
 }
