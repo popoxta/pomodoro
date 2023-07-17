a2263d2f61cebce4b396ee25d9d5cb50d7327046
popoxta <popoxta@gmail.com>
2023-07-17 13:33:27 +1200

    Improve styles

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,7 @@
 export default function Header() {
     return (
-        <header>
-            <h1 className={'text-6xl font-bold text-slate-900'}>Pomo Buddy</h1>
+        <header className={'mt-16'}>
+            <h1 className={'text-5xl tracking-wide font-bold text-slate-900'}>Pomo Buddy</h1>
             <h2 className={'font-medium text-lg text-red-700 ml-1 -mt-1.5'}>Your little tomato study-buddy</h2>
         </header>
     )
--- src/components/Tomato.tsx
+++ src/components/Tomato.tsx
@@ -4,10 +4,10 @@ export default function Tomato() {
         work: '../../tomato-work.png'
     }
     return (
-        <section className={'flex flex-row gap-3 justify-center place-items-center'}>
-            <img className={'w-52'} src={tomatoes.work} alt={'Cute Pomodoro'}/>
-            <div className={'bg-slate-200 mt-3 p-5 rounded-lg max-w-xs'}>
-                <p>Motivational quote placeholderMotivational quote placeholderMotivational quoter</p>
+        <section className={'flex flex-row gap-5 justify-center place-items-center mt-24 mb-20'}>
+            <img className={'w-60'} src={tomatoes.work} alt={'Cute Pomodoro'}/>
+            <div className={'bg-slate-200 mt-3 px-6 py-7 rounded-lg max-w-xs'}>
+                <p>Motivational quote placeholderMotivational quote placeholder Motivational quoter</p>
             </div>
         </section>
     )
