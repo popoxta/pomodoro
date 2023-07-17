9c161f6e4988c1294105568d9a127bd42b5351ec
popoxta <popoxta@gmail.com>
2023-07-17 14:22:22 +1200

    Add timer functionality

--- src/components/Timer.tsx
+++ src/components/Timer.tsx
@@ -1,25 +1,42 @@
 import {useEffect, useState} from "react";
 
+const ONE_SECOND: 1000 = 1000
+const ONE_MINUTE: number = 60 * ONE_SECOND
+
+const POMO_TIME: number = 25 * ONE_MINUTE
+const LONG_BREAK: number = 15 * ONE_MINUTE
+const SHORT_BREAK: number = 5 * ONE_MINUTE
+
 export default function Timer() {
-    const ONE_SECOND = 1000
-    const ONE_MINUTE = 60 * ONE_SECOND
-
-    const POMO_TIME: number = 25 * ONE_MINUTE
-    const LONG_BREAK: number = 15 * ONE_MINUTE
-    const SHORT_BREAK: number = 5 * ONE_MINUTE
-
-    const [time, setTime] = useState(POMO_TIME)
+    const [timeRemaining, setTimeRemaining] = useState(POMO_TIME)
     const [pomosElapsed, setPomosElapsed] = useState(0)
     const [isPaused, setIsPaused] = useState(true)
-    
-   console.log(time)
+
+    const formattedTimeRemaining: Date = new Date(timeRemaining)
+    const minutes: string = String(formattedTimeRemaining.getMinutes())
+    const seconds: string = String(formattedTimeRemaining.getSeconds()).padStart(2, "0")
+
+    useEffect(() => {
+        if (!isPaused && timeRemaining > 0) {
+            const timer: number = setInterval(
+                () => setTimeRemaining((prev: number) => prev - ONE_SECOND),
+                1000
+            )
+            return () => clearInterval(timer)
+        }
+    }, [isPaused])
+
+    const togglePause = () => setIsPaused(!isPaused)
 
     return (
         <section className={'flex flex-col place-items-center'}>
             <div>
                 <h3 className={'text-red-700 text-3xl font-bold'}>
-                    12:26
-                    <i className={'fa-solid fa-play ml-3 text-slate-400 hover:text-slate-500 cursor-pointer transition-colors'}/>
+                    {minutes}:{seconds}
+                    <i
+                        className={`ml-3 text-slate-400 hover:text-slate-500 cursor-pointer transition-colors 
+                        fa-solid ${isPaused ? 'fa-play' : 'fa-pause'}`}
+                        onClick={togglePause}/>
                 </h3>
             </div>
             <hr className={'w-64 h-0.5 border-slate-300 my-5'}/>
